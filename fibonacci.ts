4153b6226c57661c74b230a5af2ca998bbc7ab08
leojpod <leo.jpod@gmail.com>
2016-06-12 20:41:16 +0200

    typscript update

--- fibonacci.ts
+++ fibonacci.ts
@@ -7,21 +7,23 @@
  * @param {int} n
  * @return {int}
  */
-function fibo(n) {
+function fibo(n: number): number {
+  'use strict';
+
   if (n < 1) {
     throw 'huston we have a problem';
   }
-  if (n == 1) {
+  if (n === 1) {
     return 0;
   }
-  if (n == 2) {
+  if (n === 2) {
     return 1;
   }
   return fibo(n - 1) + fibo(n - 2);
 }
 
-process.on('message', function (n) {
+process.on('message', function (n: number): void {
   console.log('computing fibo(%s)', n);
-  var f = fibo(n);
+  let f: number = fibo(n);
   process.send(f);
 });
--- index.ts
+++ index.ts
@@ -2,14 +2,15 @@
  * Created by leojpod on 3/6/16.
  */
 
-var fork = require('child_process').fork;
+import {fork} from 'child_process';
+import {ChildProcess} from 'child_process';
 
-module.exports = function (n, cb) {
-  var fiboComputer = fork(__dirname + '/fibonacci');
-  fiboComputer.on('message', function (msg) {
+export function fibo(n: number, cb: {(fibOfN: number): void}): void {
+  'use strict';
+  let fiboComputer: ChildProcess = fork(__dirname + '/fibonacci');
+  fiboComputer.on('message', function (msg: string): void {
     console.log('fibo of %s is %s', n, msg);
-    cb(msg);
+    cb(Number.parseInt(msg));
   });
-
   fiboComputer.send(n);
-};
+}
